ec1c48d6f65c13cb3b68543d8191997303fb26e3
RuthIsRoot <98743413+RuthIsRoot@users.noreply.github.com>
2022-04-05 20:42:17 +0200

    Create myfirstplugin.php

---
+++ myfirstplugin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Alex - My First Plugin
+ * Plugin URI: 
+ * Description: The very first plugin that I have ever created.
+ * Version: 1.0
+ * Author: Alex Castella Iniesta
+ * Author URI: http://www.alexwordpress.com.mialias.net/
+ * License: GPLv3
+ */
+
+?>
