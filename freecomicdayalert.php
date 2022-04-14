d01c5f3eab9828d2c64120af9091345816ea7237
RuthIsRoot <98743413+RuthIsRoot@users.noreply.github.com>
2022-04-14 22:56:34 +0200

    Update freecomicdayalert.php

--- freecomicdayalert.php
+++ freecomicdayalert.php
@@ -1,23 +1,38 @@
 <?php
 /**
- * Plugin Name: FreeComicDay Alert
- * Plugin URI: https://github.com/RuthIsRoot/FreeComicDay-Alert
- * Description: Este plugin informa al usuario mediante un alert de JS si hoy es el día mundial del FreeComicDay (06/05/2023) al llegar a este día, se le informa al usuario y se le adjunta un enlace donde puede obtener más información.
+ * Plugin Name: Check Events Day Alert
+ * Plugin URI: https://github.com/RuthIsRoot/Check Events Day Alert/
+ * Description: Plugin que contiene un array con información sobre eventos importantes en diferentes paises. Si la fecha de hoy corresponde a algún evento, cada vez que el usuario accede a la página o se mueve por ella se le notifica de esto mediante un alert de JS.
  * Version: 1.0
  * Author: Alex Castella Iniesta
  * Author URI: http://www.alexwordpress.com.mialias.net/
  * License: GPLv3
  */
 
-add_action('wp_head','checkFreeComicDay');
+add_action('wp_enqueue_scripts','checkEventsDay');
 
-function checkFreeComicDay() {
+function checkEventsDay() {
 	$today = date('d/m/Y');
-	$freeComicDayDate = "06/05/2023";
-	
-	if ($today == $freeComicDayDate) {
-		echo '<script type="text/javascript">alert("Sabías que hoy, 06/05/2023 es el día del Free Comic Book ?\n\nVisita https://www.freecomicbookday.com/ para obtener más información ;)")</script>';
+
+	$events = [
+	    "14/04/2022" => "Sabías que hoy, 23/04/2022 es el día de la GERMAN COMIC CON ? Visita https://www.feriasinfo.es/Ferias-de-comic-Y406-S1.html para obtener más información ;)",
+	    // 14/04/2022 Es una prueba
+	    "23/04/2022" => "Sabías que hoy, 23/04/2022 es el día de la GERMAN COMIC CON ? Visita https://www.feriasinfo.es/Ferias-de-comic-Y406-S1.html para obtener más información ;)",
+	    "14/05/2022" => "Sabías que hoy, 14/05/2022 es el día de la VIENNA COMIX ? Visita https://www.feriasinfo.es/Ferias-de-comic-Y406-S1.html para obtener más información ;)",
+	    "26/05/2022" => "Sabías que hoy, 26/05/2022 es el día de la FANTASY BASEL COMIC CON ? Visita https://www.feriasinfo.es/Ferias-de-comic-Y406-S1.html para obtener más información ;)",
+	    "05/08/2022" => "Sabías que hoy, 05/08/2022 es el día de la CHICAGO COMIC & ENTERTAINMENT EXPO ? Visita https://www.feriasinfo.es/Ferias-de-comic-Y406-S1.html para obtener más información ;)",
+	    "06/10/2022" => "Sabías que hoy, 06/10/2022 es el día del NEW YORK COMIC CON ? Visita https://www.feriasinfo.es/New-York-Comic-Con-M3225/Nueva-York.html para obtener más información ;)",
+	];
+
+	foreach($events as $day => $info) {
+	    if ($today == $day) {
+	    echo '<script type="text/javascript">/'.'/ <![CDATA[
+		    window.onload = function(){
+		      alert("'. $info.'");
+		    }
+		  /'.'/ ]]>
+		  </script>';
+	    }
 	}
 }
-
 ?>
